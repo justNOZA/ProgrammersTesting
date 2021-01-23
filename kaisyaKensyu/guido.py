76f7891e72eab80f28de22cf85a4741414b4f550
justNOZA <1995pjs2635@gmail.com>
2021-01-23 12:17:30 +0900

    kensyuu

---
+++ kaisyaKensyu/guido.py
@@ -0,0 +1,26 @@
+print("hello"+"world")
+print("hello "*5)
+print('5'+'99')
+print(int('5')+int('99'))
+print(5+99)
+print(str(5)+str(99))
+
+print('パク')
+#_*__coding:UTF-8 _*_
+print("박재성"
+a = 'a'
+print(a)
+
+tokyoToNagoya = 365
+speed = 90
+print(str(tokyoToNagoya/speed)+'km')
+
+top = int(input('상단의 길이를 입력해주세요:'))
+low = int(input('하단의 길이를 입력해 주세요:'))
+high = int(input('높이를 입력해주세요:'))
+result = (top+low)*high/2
+print('台形の面積は','です。',sep=str(result))
+
+wage = int(input('How much is your hourly wage?:'))
+time = int(input('How many hours did you work?:'))
+print('あなたの今月の給与は','です。',sep=str(wage*time))
