fec76917423b492cf2e51318244dc07813ead821
justNOZA <1995pjs2635@gmail.com>
2021-01-23 13:49:14 +0900

    CalendarTEst

---
+++ ProgrammersTestingFolder/CalendarTest.py
@@ -0,0 +1,8 @@
+import calendar
+
+def solution(a, b):
+    day ={0 : 'MON', 1 : 'TUE', 2 : 'WED', 3 : 'THU', 4 : 'FRI', 5 : 'SAT', 6 : 'SUN'}
+    answer = day[calendar.weekday(2016, 1, 5)]
+    print(answer)
+
+solution(2,11)
