68cc61865716ac54b4dfa276f7776d4baaed15f2
justNOZA <1995pjs2635@gmail.com>
2021-07-02 20:31:55 +0900

    programmers hash2

--- ProgrammersTestingFolder/List01.py
+++ ProgrammersTestingFolder/List01.py
@@ -15,3 +15,39 @@ def solution(array, commands):
     for com in commands:
         answer.append(sorted(array[com[0]-1:com[1]])[com[2]-1])
     return answer
+
+def solution(list):
+     answer = True
+     check = False
+     list = sorted(list, key=len)
+     for i in range(0, len(list)):
+        if check:
+             break
+        current = list[i]
+        for j in range(i+1, len(list)):
+            comp = list[j]
+            if len(current)<len(comp) and current == comp[0:len(current)]:
+                answer = False
+                check = True 
+                break
+    return answer
+
+
+class Solution {
+    public boolean solution(String[] pb) {
+        boolean a = true;
+
+        for(String b : pb){
+            int i = 0;
+            for(String c : pb){
+                if(c.indexOf(b) == 0){
+                    i++;
+                }
+                if(i == 2){
+                    return false;
+                }
+            }
+        }
+        return a;
+    }
+}
